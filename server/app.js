7778eacbdc5c3b4f002288aa3f669bdd5b620115
joel-rodgers <joel.kobara@strathmore.edu>
2022-11-16 00:08:08 +0300

    Registration API created

--- server/app.js
+++ server/app.js
@@ -7,11 +7,52 @@ const cookieParser = require('cookie-parser');
 
 const app = express();
 
+// Configure ENV File & Require Connection File
+dotenv.config({path : './config.env'});
+require('./db/conn');
+const port = process.env.PORT; 
+
+// Require Model
+const Users = require('./models/userSchema');
+
+// These Method is Used to Get Data and Cookies from FrontEnd
+app.use(express.json());
+app.use(express.urlencoded({extended : false}));
+app.use(cookieParser());
+
 app.get('/', (req, res) => {
     res.send("Hello World");
 })
 
+// Registration
+app.post('/register', async (req, res) => {
+    try {
+        // Get body or Data
+        const firstname = req.body.firstname;
+        const surname = req.body.surname;
+        const email = req.body.email;
+        const password = req.body.password;
+        
+        const createUser = new Users({
+            firstname : firstname,
+            surname : surname,
+            email : email,
+            password : password
+        });
+
+        //Save Method is Used to Create User or Insert User
+        //But Before Saving or Inserting, Password will Hash
+        //Because of Hashing. After Hash, It will save to DB
+        const created = await createUser.save();
+        console.log(created);
+        res.status(200).send("Registered");
+
+    } catch (error) {
+        res.status(400).send(error)
+    }
+})
+
 // Run Server
-app.listen(3001, ()=>{
+app.listen(port, ()=>{
     console.log("Server is Listening")
 })
